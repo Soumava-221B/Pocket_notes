d3cc2e88855611f0c6dde1c5a23b58e1d7a3bab2
soumava <soumavadas06@gmail.com>
2024-01-29 19:39:18 +0530

    updated index js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from '../src/pages/home/HomePage'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <HomePage/>
     </div>
   );
 }
